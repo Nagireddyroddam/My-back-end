a4508320f2375d9caaf27f5c4640b1d41fd9e1a5
Nagireddy Roddam <roddamnagireddy1@gmail.com>
2026-05-18 10:29:16 +0530

    Delete task through data base

--- index.js
+++ index.js
@@ -24,7 +24,12 @@ app.post('/tasks', async (req, res) => {
   const allTasks = await pool.query('SELECT * FROM tasks');
   res.json(allTasks.rows);
 });
-
+app.delete('/tasks/:id', async (req, res) => {
+  const id = req.params.id;
+  await pool.query('DELETE FROM tasks WHERE id = $1', [id]);
+  const allTasks = await pool.query('SELECT * FROM tasks');
+  res.json(allTasks.rows);
+});
 app.listen(5000, () => {
   console.log('Server running on port 5000');
 });
