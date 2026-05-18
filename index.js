4f6f4bbd054c873aab28f9fcc90d29cfabf567ce
Nagireddy Roddam <roddamnagireddy1@gmail.com>
2026-05-18 09:01:09 +0530

    connected PostgreSQL to Express backend

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'todoapp',
+  password: 'P@SS_wo6d',
+  port: 5432,
+});
 
 app.use(express.json());
 app.use(cors());
 
-let tasks = ['Wake up', 'Study React'];
-
-app.get('/tasks', (req, res) => {
-  res.json(tasks);
+app.get('/tasks', async (req, res) => {
+  const result = await pool.query('SELECT * FROM tasks');
+  res.json(result.rows);
 });
 
-app.post('/tasks', (req, res) => {
-  const newTask = req.body.task;
-  tasks.push(newTask);
-  res.json(tasks);
+app.post('/tasks', async (req, res) => {
+  await pool.query('INSERT INTO tasks (task) VALUES ($1)', [req.body.task]);
+  const allTasks = await pool.query('SELECT * FROM tasks');
+  res.json(allTasks.rows);
 });
 
 app.listen(5000, () => {
