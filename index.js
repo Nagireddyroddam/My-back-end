089e276e11c1bb665775ef705789e9bcc7c0a0ca
Nagireddy Roddam <roddamnagireddy1@gmail.com>
2026-05-20 06:56:49 +0530

    added mongoDB connection and routes

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const pool = new Pool({
   password: 'P@SS_wo6d',
   port: 5432,
 });
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/todoapp');
+const taskSchema = new mongoose.Schema({
+  task: String
+});
+
+const Task = mongoose.model('Task', taskSchema);
 
 app.use(express.json());
 app.use(cors());
@@ -29,6 +37,16 @@ app.delete('/tasks/:id', async (req, res) => {
   await pool.query('DELETE FROM tasks WHERE id = $1', [id]);
   const allTasks = await pool.query('SELECT * FROM tasks');
   res.json(allTasks.rows);
+});
+app.get('/mongo-tasks', async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+app.post('/mongo-tasks', async (req, res) => {
+    const newTask = new Task({ task: req.body.task });
+  await newTask.save();
+  const allTasks = await Task.find();
+  res.json(allTasks);
 });
 app.listen(5000, () => {
   console.log('Server running on port 5000');
