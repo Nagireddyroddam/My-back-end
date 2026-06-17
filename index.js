421c23b9ffbac4a58d9447bc983675e2610be7ea
Nagireddy Roddam <roddamnagireddy1@gmail.com>
2026-06-17 08:23:04 +0530

    use environment variables for deployment

--- index.js
+++ index.js
@@ -10,20 +10,12 @@ const pool = new Pool({
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
 });
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://Nagireddy:P%40SS_wo6d@cluster0.fir0ilj.mongodb.net/todoapp?appName=Cluster0');
-const taskSchema = new mongoose.Schema({
-  task: String
-});
-
-const Task = mongoose.model('Task', taskSchema);
 
 app.use(express.json());
 app.use(cors());
+
 const { router: authRoutes, verifyToken } = require('./auth');
 app.use('/auth', authRoutes);
-
 
 app.get('/tasks', verifyToken, async (req, res) => {
   const result = await pool.query('SELECT * FROM tasks');
@@ -35,22 +27,15 @@ app.post('/tasks', async (req, res) => {
   const allTasks = await pool.query('SELECT * FROM tasks');
   res.json(allTasks.rows);
 });
+
 app.delete('/tasks/:id', async (req, res) => {
   const id = req.params.id;
   await pool.query('DELETE FROM tasks WHERE id = $1', [id]);
   const allTasks = await pool.query('SELECT * FROM tasks');
   res.json(allTasks.rows);
 });
-app.get('/mongo-tasks', async (req, res) => {
-  const tasks = await Task.find();
-  res.json(tasks);
-});
-app.post('/mongo-tasks', async (req, res) => {
-    const newTask = new Task({ task: req.body.task });
-  await newTask.save();
-  const allTasks = await Task.find();
-  res.json(allTasks);
-});
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
